317f63867f22863996e7e67cea5ee88aed6b1c3a
Dave Ward <professorx00@gmail.com>
2019-09-28 20:19:29 -0400

    fixed validation

--- public/assets/scripts/survey.js
+++ public/assets/scripts/survey.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
     $('select').formSelect();
-    $('.modal').modal();    
+    $('.modal').modal();
     const form = $("#surveyForm");
     const name = $("#name");
     const photo = $("#photo");
@@ -16,15 +16,32 @@ $(document).ready(function () {
     const questionNine = $("#questionNine");
     const questionTen = $("#questionTen");
     const btn = $("#formSubmit");
-    const resetBtn = $("#close");
-    let allquestions = []
+
+    function validateQuestions() {
+        let formValid = false;
+        
+        if(questionOne.val() && questionTwo.val() && questionThree.val() && questionFour.val() && questionFive.val() && questionSix.val() && questionSeven.val() && questionEight.val()  && questionNine.val() && questionTen.val() ){
+            formValid = true
+        }
+        if(formValid){
+            let allquestions = `${questionOne.val()},${questionTwo.val()},${questionThree.val()},${questionFour.val()},${questionFive.val()},${questionSix.val()},${questionSeven.val()},${questionEight.val()},${questionNine.val()},${questionTen.val()}`
+            // addFriend(combineAnswers(allquestions))
+            console.log("Passed")
+        }
+        else{
+            $("#error").text("Please Check that you answered all the questions");
+            console.log("failed")
+        }
+
+    }
+
     function combineAnswers(answers) {
         return {
             "name": name.val().trim(),
             "photo": photo.val().trim(),
             "questions": answers.trim()
         }
-    
+
     };
     function addFriend(answers) {
         $.post('/api/addFriend', answers, function (res) {
@@ -34,23 +51,21 @@ $(document).ready(function () {
             console.log("friend found")
             let newDiv = $("<div>")
             let char = $("<h2>").text(res.name)
-            let charImg = $("<img>").attr("src",res.photo).addClass("modalImages")
-            newDiv.append(char,charImg)
+            let charImg = $("<img>").attr("src", res.photo).addClass("modalImages")
+            newDiv.append(char, charImg)
             $("#modalContent").append(newDiv)
             $('.modal').modal('open')
-            $("#rest").on("click",function(event){
+            $("#rest").on("click", function (event) {
                 event.preventDefault();
                 $('#surveyForm').trigger("reset");
             })
         });
     }
-    
+
     btn.on("click", function (event) {
         event.preventDefault()
         console.log("click")
-        allquestions = `${questionOne.val()},${questionTwo.val()},${questionThree.val()},${questionFour.val()},${questionFive.val()},${questionSix.val()},${questionSeven.val()},${questionEight.val()},${questionNine.val()},${questionTen.val()}`
-        let test = combineAnswers(allquestions)
-        addFriend(test)
+        validateQuestions();
     })
 
 });
